5683efeb6dbc26bf0b89bfac1f4675c30ce610e6
Jeovana Miranda <jeovanamiranda96@gmail.com>
2025-11-21 18:45:20 -0300

    Atualiza a entidade com o novo atributo

--- src/Nutra.Domain/Entidades/TipoRegistro.cs
+++ src/Nutra.Domain/Entidades/TipoRegistro.cs
@@ -4,13 +4,15 @@ namespace Nutra.Domain.Entidades;
 public class TipoRegistro : Entity
 {
     public CategoriaRegistro Categoria { get; set; }
+    
+    public TipoDetalhe TipoDetalhe { get; set; }
     public string? Descricao { get; set;}
 
     public List<Registros> Registros { get; set; } = new List<Registros>();
     
     public TipoRegistro(){ }
 
-    public TipoRegistro(CategoriaRegistro categoria, string? descricao)
+    public TipoRegistro(CategoriaRegistro categoria, string? descricao, TipoDetalhe tipoDetalhe)
     {
         Categoria = categoria;
         Descricao = descricao;
