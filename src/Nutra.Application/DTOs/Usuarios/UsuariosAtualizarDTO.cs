b5c49e314020648fa42750c1a65dab1fd15ed68c
Jeovana Miranda <jeovanamiranda96@gmail.com>
2025-11-24 00:28:08 -0300

    atualiza dtos

---
+++ src/Nutra.Application/DTOs/Usuarios/UsuariosAtualizarDTO.cs
@@ -0,0 +1,9 @@
+namespace Nutra.Application.DTOs;
+
+public class UsuariosAtualizarDTO
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Nome { get; set; }
+    public string Turma { get; set; }
+}
---
+++ src/Nutra.Application/DTOs/Usuarios/UsuariosCriarDTO.cs
@@ -0,0 +1,9 @@
+namespace Nutra.Application.DTOs;
+
+public class UsuariosCriarDTO
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Tipo { get; set; } = "Aluno";
+    public string Turma { get; set; } = string.Empty;
+}
